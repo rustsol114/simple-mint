ea91ee2103ccf3feb1180f56052b780ce4c3a2c1
rustsol114 <jamesybarra114@gmail.com>
2024-12-10 19:32:33 +0900

    update: rpc

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SOL_PRICE_API =
 // export const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC ?? "";
 // export const SOLANA_RPC = "https://solana-devnet.g.alchemy.com/v2/nPdtpY0LxgpMlnGOA94LoTCpEy-Nd2gG";
 // export const SOLANA_RPC = "https://devnet.helius-rpc.com/?api-key=934757b5-6bfc-49d7-a577-b40b81662855";
-export const SOLANA_RPC = "https://mainnet.helius-rpc.com/?api-key=934757b5-6bfc-49d7-a577-b40b81662855"
+export const SOLANA_RPC = "https://mainnet.helius-rpc.com/?api-key=67cdd4a6-271e-4635-b0d3-6d007ef93fa8"
 // export const SOLANA_RPC = "https://solana-mainnet.g.alchemy.com/v2/nPdtpY0LxgpMlnGOA94LoTCpEy-Nd2gG";
 
 
